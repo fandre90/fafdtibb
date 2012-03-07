9e5388f8faaab93ea5165d3d029da8f92fc870be
mbarthel <mbarthel@insa-rennes.fr>
2012-03-07 19:19:53 +0000

    fonction static pr convertir l'attribut en entier!

--- src/fr/insarennes/fafdti/builder/AttrType.java
+++ src/fr/insarennes/fafdti/builder/AttrType.java
@@ -1,5 +1,18 @@
 package fr.insarennes.fafdti.builder;
 
+import fr.insarennes.fafdti.FAFException;
+
 public enum AttrType {
 	CONTINUOUS, TEXT, DISCRETE;
+	
+	public static AttrType getFromString(String type) throws FAFException{
+		if(type.equals("CONTINUOUS"))
+			return CONTINUOUS;
+		else if(type.equals("TEXT"))
+			return TEXT;
+		else if(type.equals("DISCRETE"))
+			return DISCRETE;
+		else
+			throw new FAFException("ERROR : Ask AttrType from unknown String");
+	}
 }

b73340503d1104e08b3c72d516ec057ce8ae71aa
pwilke <pwilke@insa-rennes.fr>
2012-05-25 13:26:09 +0000

    Ajout du critère de Gini, Youhou !

---
+++ src/fr/insarennes/fafdti/builder/GiniCriterion.java
@@ -0,0 +1,21 @@
+package fr.insarennes.fafdti.builder;
+
+public class GiniCriterion extends Criterion {
+
+	@Override
+	public boolean better(double value1, double value2) {
+		return value1 < value2;
+	}
+
+	@Override
+	public double compute(int[] distributionVector) {
+		double criterionValue = 0;
+		//calcul de la somme des ni²
+		double N = 0;
+		for (int i = 0; i < distributionVector.length; i++)
+			N += Math.pow(distributionVector[i],2);
+		criterionValue = 1 - N;
+		return criterionValue;
+	}
+
+}

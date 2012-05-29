eb3fbfebf46fc2d3c47eb8347b2ae9bca9e46376
fandre <fandre@insa-rennes.fr>
2012-05-29 14:10:54 +0000

    Ajout de logging dans le combiner pour faciliter le débogage des OutOfMemoryError

--- src/fr/insarennes/fafdti/hadoop/furious/Step1Combiner.java
+++ src/fr/insarennes/fafdti/hadoop/furious/Step1Combiner.java
@@ -3,6 +3,7 @@ package fr.insarennes.fafdti.hadoop.furious;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.log4j.Logger;
 
 import fr.insarennes.fafdti.FAFException;
 import fr.insarennes.fafdti.hadoop.ReducerBase;
@@ -12,6 +13,8 @@ import fr.insarennes.fafdti.hadoop.WritableValueSDVSortedMap;
 public class Step1Combiner
 		extends
 		ReducerBase<IntWritable, WritableValueSDVSortedMap, IntWritable, WritableValueSDVSortedMap> {
+	private Logger log = Logger.getLogger("COMB: ");
+	private Runtime run = Runtime.getRuntime();
 
 	protected void reduce(
 			IntWritable col,
@@ -21,6 +24,14 @@ public class Step1Combiner
 			ValueDistributionMapAggregator valueDistMapAgg = 
 					new ValueDistributionMapAggregator();
 			valueDistMapAgg.aggregateAll(valueDistMaps);
+			/*
+			log.info("Attr: " + col.get() + " - Count / Size : " + 
+					valueDistMapAgg.getNumberOfAggregatedMaps() + " / " +
+					valueDistMapAgg.getSize());
+			log.info("Used / Total: " + ((run.totalMemory() - run.freeMemory()) / (1024 * 1024)) + " / " +
+					(run.totalMemory() / (1024 * 1024)));
+			*/
+			//System.gc();
 			context.write(col, valueDistMapAgg.getAggregatedMap());
 		} catch (FAFException e) {
 			e.printStackTrace();
